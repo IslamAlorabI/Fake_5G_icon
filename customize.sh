d58ce083b254ad0c2546f3744a39876da3c80a54
Islam AlorabI <51124058+IslamAlorabI@users.noreply.github.com>
2025-06-29 10:52:29 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,104 +1,136 @@
 #!/system/bin/sh
+#
+# Universal logger function
+# This will print to both the recovery console and the installation log
+ui_print() {
+  echo "$1"
+}
+
+# Set to true to skip extraction of the module zip
 SKIPUNZIP=1
 
+# Extract module files
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
+# --- Start of Language and String Definitions ---
 # Default lang en_US
-LANG_VOLUME_KEY_ERROR="- Volume key error!"
-LANG_VOLUME_KEY_AGAIN="- Volume key not detected. Try again"
-LANG_SELECT_ATYLE="- Select Style: 5G or 5GE "
-LANG_SELECT_ATYLE_5G="- Vol Up = 5G "
-LANG_SELECT_ATYLE_5GE="- Vol Down = 5GE "
-LANG_SELECTED_5G="- Selected 5G "
-LANG_SELECTED_5GE="- Selected 5GE "
-LANG_WARNING="- Your device is not supported ! "
+LANG_VOLUME_KEY_ERROR="- Volume key error! Installation aborted."
+LANG_VOLUME_KEY_AGAIN="- Volume key not detected. Please try again."
+LANG_SELECT_ATYLE="- Select Style: 5G or 5GE"
+LANG_SELECT_ATYLE_5G="- Vol Up = 5G"
+LANG_SELECT_ATYLE_5GE="- Vol Down = 5GE"
+LANG_SELECTED_5G="- Selected 5G Style"
+LANG_SELECTED_5GE="- Selected 5GE Style"
+LANG_WARNING="- Your device is not supported! Could not find SystemUI.apk."
+# --- End of Language and String Definitions ---
 
-# Local lang
-locale=$(getprop persist.sys.locale|awk -F "-" '{print $1"_"$NF}')
-[[ ${locale} == "" ]] && locale=$(settings get system system_locales|awk -F "," '{print $1}'|awk -F "-" '{print $1"_"$NF}')
-if [ ${locale} = "zh_CN" ];then
-   . "$MODPATH/${locale}.ini"
+# Load local language if available
+locale=$(getprop persist.sys.locale | awk -F "-" '{print $1"_"$NF}')
+[ "$locale" = "" ] && locale=$(settings get system system_locales | awk -F "," '{print $1}' | awk -F "-" '{print $1"_"$NF}')
+if [ -f "$MODPATH/${locale}.ini" ]; then
+  . "$MODPATH/${locale}.ini"
 fi
 
-chmod -R 0755 $MODPATH/file/tools
-P7Z=$MODPATH/file/tools/7za
+# Set permissions for tools
+chmod -R 0755 "$MODPATH/file/tools"
+P7Z="$MODPATH/file/tools/7za"
 
+#
+# Universal Volume Key Selector for Magisk & KernelSU
+# This function replaces the non-portable keycheck binary
+#
 chooseport() {
-  # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
-  # Calling it first time detects previous input. Calling it second time will do what we want
-  [ "$1" ] && local delay=$1 || local delay=3
   local error=false
   while true; do
-    timeout 0 $MODPATH/file/tools/$ARCH32/keycheck
-    timeout $delay $MODPATH/file/tools/$ARCH32/keycheck
-    local SEL=$?
-    if [ $SEL -eq 42 ]; then
-      return 0
-    elif [ $SEL -eq 41 ]; then
-      return 1
-    else
-      $error && abort "${LANG_VOLUME_KEY_ERROR}"
-      error=true
-      echo "${LANG_VOLUME_KEY_AGAIN}"
+    ui_print " "
+    ui_print "${LANG_SELECT_ATYLE}"
+    ui_print "${LANG_SELECT_ATYLE_5G}"
+    ui_print "${LANG_SELECT_ATYLE_5GE}"
+    
+    # Use Android's built-in getevent tool for compatibility
+    # Wait for a key press for 3 seconds
+    timeout 3 /system/bin/getevent -lqc 1 2>/dev/null | grep 'KEY_VOLUME' > "$TMPDIR/events"
+    
+    # Check if the temporary file has any content
+    if [ -s "$TMPDIR/events" ]; then
+      if grep -q 'KEY_VOLUMEUP' "$TMPDIR/events"; then
+        rm -f "$TMPDIR/events"
+        return 0 # Return 0 for Volume Up
+      elif grep -q 'KEY_VOLUMEDOWN' "$TMPDIR/events"; then
+        rm -f "$TMPDIR/events"
+        return 1 # Return 1 for Volume Down
+      fi
     fi
+    
+    # If no valid key was detected after the timeout
+    $error && abort "${LANG_VOLUME_KEY_ERROR}"
+    error=true
+    ui_print "${LANG_VOLUME_KEY_AGAIN}"
   done
 }
 
-ui_print "${LANG_SELECT_ATYLE}"
-ui_print "${LANG_SELECT_ATYLE_5G}"
-ui_print "${LANG_SELECT_ATYLE_5GE}"
+# --- Main Script Logic ---
+
+# Let the user choose the style
 if chooseport; then
   ui_print "${LANG_SELECTED_5G}"
-  addfile=$MODPATH/file/5G
+  addfile="$MODPATH/file/5G"
 else
   ui_print "${LANG_SELECTED_5GE}"
-  addfile=$MODPATH/file/5GE
+  addfile="$MODPATH/file/5GE"
 fi
 
-# Find dir
+ui_print " "
+ui_print "- Searching for SystemUI.apk..."
+
+# Find the target APK directory
+SYSTEMUI_PATH=""
 for partition in system vendor product system_ext; do
-   find /${partition} -name SystemUI.apk > $MODPATH/dir.txt
-   find_dir_text=$(cat $MODPATH/dir.txt | wc -l)
-   if [ ${find_dir_text} -eq 0 ]; then
-      rm -rf $MODPATH/dir.txt
-      find /${partition} -name *SystemUI.apk > $MODPATH/dir.txt
-      find_dir_text=$(cat $MODPATH/dir.txt | wc -l)
-      if [ ${find_dir_text} -eq 0 ]; then
-         rm -rf $MODPATH/dir.txt
-         find /${partition} -name SystemUI*.apk > $MODPATH/dir.txt
-         find_dir_text=$(cat $MODPATH/dir.txt | wc -l)
-      fi
-   fi
-   if [ ${find_dir_text} -gt 0 ]; then
-   break
-   fi
+  # Search for common naming conventions of SystemUI.apk
+  for name in "SystemUI.apk" "*SystemUI.apk" "SystemUI*.apk"; do
+    # find can return multiple results, we only need the first one
+    SYSTEMUI_PATH=$(find "/${partition}" -type f -name "${name}" 2>/dev/null | head -n 1)
+    if [ -n "$SYSTEMUI_PATH" ]; then
+      break 2 # Break out of both loops
+    fi
+  done
 done
-if [ ${find_dir_text} -eq 0 ]; then
-    abort "${LANG_WARNING}"
+
+if [ -z "$SYSTEMUI_PATH" ]; then
+  abort "${LANG_WARNING}"
 fi
 
-# Inject file
-apkname=$(cat $MODPATH/dir.txt | awk -F '/' '{print $NF}')
-sed -i 's/\/'$apkname'//' $MODPATH/dir.txt
-dir=$(sed -n '1p' $MODPATH/dir.txt)
-if [ `grep -c "vendor" $MODPATH/dir.txt` -ne '0' ]; then
-   mkdir -p $MODPATH/system$dir
-elif [ `grep -c "product" $MODPATH/dir.txt` -ne '0' ]; then
-   mkdir -p $MODPATH/system$dir
-elif [ `grep -c "system_ext" $MODPATH/dir.txt` -ne '0' ]; then
-   mkdir -p $MODPATH/system$dir
-else
-   mkdir -p $MODPATH$dir
-fi
+ui_print "- Found at: ${SYSTEMUI_PATH}"
 
-cp -f $dir/$apkname $TMPDIR/SystemUI.zip
-$P7Z a $TMPDIR/SystemUI.zip $addfile/res
-cp $TMPDIR/SystemUI.zip $MODPATH$dir/$apkname
+# Prepare paths for modification
+apkname=$(basename "$SYSTEMUI_PATH")
+dir=$(dirname "$SYSTEMUI_PATH")
+MOD_DIR_SYSTEMUI="$MODPATH/system$dir"
+
+# Create the necessary directory structure in the module
+mkdir -p "$MOD_DIR_SYSTEMUI"
+
+ui_print "- Modifying ${apkname}..."
+
+# Copy the original APK to a temporary location
+cp -f "$SYSTEMUI_PATH" "$TMPDIR/SystemUI.zip"
+
+# Use 7zip to add the new resource files into the APK archive
+# The 'a' command adds files to an archive.
+$P7Z a "$TMPDIR/SystemUI.zip" "$addfile/res" >/dev/null
+
+# Copy the modified APK into your module's directory
+cp "$TMPDIR/SystemUI.zip" "$MOD_DIR_SYSTEMUI/$apkname"
+
+ui_print "- Modification complete."
+ui_print "- Cleaning up..."
 
 # Delete unnecessary files
-rm -rf $MODPATH/file
-rm -rf $MODPATH/dir.txt
-rm -rf $MODPATH/*.ini
-rm -rf $MODPATH/*.md
+rm -rf "$MODPATH/file"
+rm -f "$MODPATH"/*.ini
+rm -f "$MODPATH"/*.md
 
+# Set permissions for the module files
 set_perm_recursive $MODPATH 0 0 0755 0644
+
+ui_print "- Done!"
